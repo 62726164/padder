823dd563ec636155f5f17f7eab1312c28b08df33
62726164 <btilley@gatech.edu>
2021-01-09 18:56:22 -0500

    add comment

--- go/fake.go
+++ go/fake.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+// func (x *Int) Int64() int64
+// Int64 returns the int64 representation of x.
+// If x cannot be represented in an int64, the result is undefined.
+
 func genFakePad(fakeMsg, cipherText string) {
 	fmt.Printf("not yet implemented\n")
 }
